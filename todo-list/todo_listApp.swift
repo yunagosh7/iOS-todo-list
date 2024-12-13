0c9f655ca93531e56b37880a376cc22089d9d149
Juan Cruz Vila <parlantephilips7@gmail.com>
2024-12-13 13:40:49 -0300

    Initial Commit

---
+++ todo-list/todo_listApp.swift
@@ -0,0 +1,17 @@
+//
+//  todo_listApp.swift
+//  todo-list
+//
+//  Created by Juan Cruz Vila on 13/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct todo_listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ todo-listTests/todo_listTests.swift
@@ -0,0 +1,17 @@
+//
+//  todo_listTests.swift
+//  todo-listTests
+//
+//  Created by Juan Cruz Vila on 13/12/2024.
+//
+
+import Testing
+@testable import todo_list
+
+struct todo_listTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
